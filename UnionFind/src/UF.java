968dc25e13b22b369dc200e75cdaea845d70861b
Gunter Goossens <gunnie.goossens@gmail.com>
2017-05-17 08:34:48 +0200

    UF class built, added main class for test

--- UnionFind/src/UF.java
+++ UnionFind/src/UF.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Created by Gunter on 17/05/2017.
  * <p>
@@ -8,6 +9,7 @@
  * <li>Find queries and union commands can be intermixed</li>
  * </ul>
  */
+
 public class UF {
 
     /**
@@ -15,8 +17,33 @@ public class UF {
      *
      * @param N number of objects
      */
-    UF(int N) {
+    private UF(int N) {
         // Initialize union-gind data structure
+    }
+
+    /**
+     * Read in number of objects N from standard input.
+     * Repeat:
+     * <ul>
+     * <li>read in pair of integers from standard input</li>
+     * <li>if they are not yet connected, connect them and print out pair</li>
+     * </ul>
+     *
+     * @param args system arguments, ignored
+     */
+
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int N = stdin.nextInt();
+        UF uf = new UF(N);
+        while (stdin.hasNext()) {
+            int p = stdin.nextInt();
+            int q = stdin.nextInt();
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                System.out.println(p + " " + q);
+            }
+        }
     }
 
     /**
@@ -25,7 +52,7 @@ public class UF {
      * @param p object 1
      * @param q object 2
      */
-    void union(int p, int q) {
+    private void union(int p, int q) {
     }
 
     /**
@@ -35,7 +62,7 @@ public class UF {
      * @param q object 2
      * @return true if p and q are connected
      */
-    boolean connected(int p, int q) {
+    private boolean connected(int p, int q) {
         return false;
     }
 
